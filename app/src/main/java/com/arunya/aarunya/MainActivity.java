11ddf217b1da054d70ded5142c438be68a893872
Hitesh1996 <chandrahitesh9@gmail.com>
2018-03-15 14:18:17 +0530

    cons_front end

--- app/src/main/java/com/arunya/aarunya/MainActivity.java
+++ app/src/main/java/com/arunya/aarunya/MainActivity.java
@@ -1,13 +1,30 @@
 package com.arunya.aarunya;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public Button pro;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pro= (Button) findViewById(R.id.button);
+        pro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openproactivity();
+            }
+        });
+    }
+    public void openproactivity(){
+        Intent in = new Intent(this,producer.class);
+        startActivity(in);
+
     }
 }
