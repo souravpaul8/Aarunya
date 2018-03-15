fd516e0737e6d82fed4f6685f999d6be1b249f8d
ankitkr602 <ankitkr602@gmail.com>
2018-03-15 15:24:37 +0530

    ankit front end with consumer

--- app/src/main/java/com/arunya/aarunya/MainActivity.java
+++ app/src/main/java/com/arunya/aarunya/MainActivity.java
@@ -1,9 +1,17 @@
 package com.arunya.aarunya;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public void consumer(View view)
+    {
+        view=(View)findViewById(R.id.consumer);
+        Intent i=new Intent(this,consumer.class);
+        startActivity(i);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
