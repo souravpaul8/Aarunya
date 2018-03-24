fc7612ec845c87d0059c9f65a232ba853573a0c4
Hitesh1996 <chandrahitesh9@gmail.com>
2018-03-24 23:51:31 +0530

    Main Activity Resolved

--- app/src/main/java/com/arunya/aarunya/MainActivity.java
+++ app/src/main/java/com/arunya/aarunya/MainActivity.java
@@ -1,20 +1,17 @@
 package com.arunya.aarunya;
 
 import android.content.Intent;
-import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends   AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        Intent in = new Intent(this,LoginActivity.class);
-        startActivity(in);
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
     }
 }
